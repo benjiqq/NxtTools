06424b8e8f96fe6407f14d8ce725c69febfb7802
benjyz <benjamin.l.cordes@gmail.com>
2014-12-05 17:41:41 +0100

    show base target

--- tools/show_blocks.py
+++ tools/show_blocks.py
@@ -1,16 +1,31 @@
 """
 show baseTarget for a number of blocks
+
 """
+
+import sys
+sys.path.append("../api") 
 
 from nxtapi import *
 
 sh = 1
 
 print '*'*20
-print '>> baseTarget %i'%sh 
+print 'block >> baseTarget delta  (starting %i)'%sh 
 print '*'*20
 
-for h in range(sh,sh+30):
-    blocks = req("getBlock",height=str(h))
-    print '%i  %s'%(h, blocks['baseTarget'])
+bts = list()
 
+numb = 20
+oldt = -1
+for h in range(sh,sh+numb):
+    blocks = req("getBlock",height=str(h))
+    #print blocks
+    t = int(blocks['timestamp'])
+    delta = t - oldt
+    oldt = t
+    bt = int(blocks['baseTarget'])
+    print '%i  %i   %i'%(h, bt, delta)
+    bts.append(bt)
+
+print sum(bts)
